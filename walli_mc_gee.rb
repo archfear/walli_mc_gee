0ab5f49490fa8f934465e569111b229e1689d97b
Dan Dofter <dan@dofter.com>
2009-04-02 13:17:04 -0700

    Fixed a lot of state related bugs.

--- walli_mc_gee.rb
+++ walli_mc_gee.rb
@@ -1,85 +1,72 @@
-require 'erb'
+# An Ali/Willo simulation bot for Twitter
+
 require 'rubygems'
 require 'sinatra'
 
-get '/' do
-  @wallimcgee ||= WalliMcGee.new
-  <<-HTML
-<html>
-  <head>
-    <title>WalliMcGee</title>
-  </head>
-  <body>
-    <h1>#{@wallimcgee.speak}</h1>
-  </body>
-</html>
-  HTML
-end
+configure do
 
-class PeepList
-  
-  def initialize(peep_hash)
-    @peeps = []
-    @weights = []
-    @total_weight = peep_hash.values.sum
-    sorted_peeps = peep_hash.sort {|a,b| b[1]<=>a[1]}
-    
-    sorted_peeps.each do |peep, weight|
-      @peeps << peep
-      @weights << weight
+  class Array
+    def to_sentence
+      case size
+        when 0 then ""
+        when 1 then self[0].to_s
+        when 2
+          "#{self[0]} and #{self[1]}"
+        else
+          "#{self[0...-1].join(', ')}, and #{self[-1]}"
+      end
     end
-  end
-  
-  def random_peeps(peep_count, excluded_peeps = [])
-    rpeeps = []
-    excluded_peeps = excluded_peeps.to_a
-    
-    peep_count.times do
-      peep = nil
-      begin
-        peep = self.random_peep
-      end while(rpeeps.include?(peep) || excluded_peeps.include?(peep))
-      
-      rpeeps << peep
-    end
-    
-    rpeeps
-  end
-  
-  def random_peep
-    @peeps[weighted_random_index]
-  end
-  
-  def weighted_random_index
-    @peeps.size.times do |x|
-      return x if rand(@total_weight) < @weights[0..x].sum
-    end
-    return 0
-  end
-  
-end
 
-class Array
-  def to_sentence
-    case size
-      when 0 then ""
-      when 1 then self[0].to_s
-      when 2
-        "#{self[0]} and #{self[1]}"
-      else
-        "#{self[0...-1].join(', ')}, and #{self[-1]}"
+    def sum
+      inject( nil ) { |sum,x| sum ? sum+x : x }
     end
   end
-  
-  def sum
-    inject( nil ) { |sum,x| sum ? sum+x : x }
-  end
-end
 
-# An Ali/Willo simulation bot for Twitter
-class WalliMcGee
+  class PeepList
+
+    def initialize(peep_hash)
+      @peeps = []
+      @weights = []
+      @total_weight = peep_hash.values.sum
+      sorted_peeps = peep_hash.sort {|a,b| b[1]<=>a[1]}
+  
+      sorted_peeps.each do |peep, weight|
+        @peeps << peep
+        @weights << weight
+      end
+    end
+
+    def random_peeps(peep_count, excluded_peeps = [])
+      rpeeps = []
+      excluded_peeps = excluded_peeps.to_a
+  
+      peep_count.times do
+        peep = nil
+        begin
+          peep = self.random_peep
+        end while(rpeeps.include?(peep) || excluded_peeps.include?(peep))
+    
+        rpeeps << peep
+      end
+  
+      rpeeps
+    end
+
+    def random_peep
+      @peeps[weighted_random_index]
+    end
+
+    def weighted_random_index
+      @peeps.size.times do |x|
+        return x if rand(@total_weight) < @weights[0..x].sum
+      end
+      return 0
+    end
+
+  end
+
   # twitter name => frequency of hangouts
-  Peeps = PeepList.new(
+  PEEP_LIST = PeepList.new(
     'aprilini' => 0.1,
     'aubs' => 0.7,
     'ceedub' => 0.1,
@@ -93,17 +80,17 @@ class WalliMcGee
     'stephdub' => 0.3,
     'willotoons' => 1.0
   )
-  
-  Phrases = [
-    'Hanging with <%=peeps%> at <%=location%>... Love my peeps!',
-    'So good to be sharing drinks with <%=peeps%> at <%=location%>. I love you guys.',
-    'Loving hanging out at <%=location%> with <%=peeps%>!',
-    'Can\'t wait to go chill with <%=peeps%> at <%=location%>. Hearts to my chubies!',
-    'Beers with <%=peeps%> at <%=location%>. W00t!',
-    'At <%=location%> with <%=peeps%>. Holla!'
+
+  PHRASES = [
+    'Hanging with <%=peeps(@speaking_as)%> at <%=location%>... Love my peeps!',
+    'So good to be sharing drinks with <%=peeps(@speaking_as)%> at <%=location%>. I love you guys.',
+    'Loving hanging out at <%=location%> with <%=peeps(@speaking_as)%>!',
+    'Can\'t wait to go chill with <%=peeps(@speaking_as)%> at <%=location%>. Hearts to my chubies!',
+    'Beers with <%=peeps(@speaking_as)%> at <%=location%>. W00t!',
+    'At <%=location%> with <%=peeps(@speaking_as)%>. Holla!'
   ]
-  
-  Locations = [
+
+  LOCATIONS = [
     'Doc\'s Clock',
     '12 Galaxies',
     'Bottom of the Hill',
@@ -111,20 +98,33 @@ class WalliMcGee
     'Zeitgeist',
     'The Hemlock'
   ]
-  
-  def speak
-    @speaking_as = %w(willotoons spangley)[rand(2)]
-    ERB.new("#{@speaking_as}: #{phrase}").result(binding)
+
+end
+
+helpers do
+  def location; LOCATIONS[rand(LOCATIONS.size)] end
+
+  def peeps(speaking_as)
+   how_many = rand(5) + 1
+   peeps = PEEP_LIST.random_peeps(how_many, speaking_as)
+   peeps.map { |name| "@#{name}" }.to_sentence
   end
-    
-  def location; Locations[rand(Locations.size)] end
+
+  def phrase; PHRASES[rand(PHRASES.size)] end
   
-  def peeps
-    how_many = rand(5) + 1
-    peeps = Peeps.random_peeps(how_many, @speaking_as)
-    peeps.map { |name| "@#{name}" }.to_sentence
-  end
+end
+
+get '/' do
+  @speaking_as = %w(willotoons spangley)[rand(2)]
   
-  def phrase; Phrases[rand(Phrases.size)] end
-  
+  erb <<-ERB
+<html>
+  <head>
+    <title>WalliMcGee</title>
+  </head>
+  <body>
+    <h1><%= @speaking_as %>: #{phrase}</h1>
+  </body>
+</html>
+  ERB
 end
